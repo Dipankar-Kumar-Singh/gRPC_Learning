bc1caf7e49daee27535cf357b6505b85c62c0f06
Dipankar <dipankarcode@gmail.com>
2023-06-27 10:28:41 +0530

    created clinet and setup reciver

--- server.js
+++ server.js
@@ -1,39 +1,53 @@
-const grpc = require('@grpc/grpc-js') ;
-const protoLoader = require('@grpc/proto-loader') 
+// const grpc = require('@grpc/grpc-js') ;
+// const protoLoader = require('@grpc/proto-loader') 
 
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
 
-const packageDef = protoLoader.loadSync("todo.proto" , {})
+const packageDef = protoLoader.loadSync("todo.proto", {})
 
+const grpcObject = grpc.loadPackageDefinition(packageDef);
 
-const grpcObject = grpc.loadPackageDefinition(packageDef) ;
-
-const todoPackage = grpc.todoPackage ;
-
-
-
-const server = new grpc.Server() ;
-server.bind("0.0.0.0:40000" , grpc.ServerCredentials.createInsecure()) ;
-
-server.addService(todoPackage.Todo.serivce ,
-    {
-        "createTodo"  : createTodo ,
-        "readTodos" : readTodos 
-    })
-
-server.start() ;
-
-console.log("packageDef : " , packageDef ) ;
-console.log("grcObjct" , grpcObject) ;
-console.log("todoPackage" , todoPackage) ;
+const todoPackage = grpcObject.todoPackage;
 
 
 
-function createTodo (call , callback) {
-    console.log(call) ;
+const server = new grpc.Server();
+
+const something = todoPackage.Todo.service ;
+
+server.addService(todoPackage.Todo.service, {
+        "createTodo": createTodo,
+        "readTodos": readTodos
+});
+
+server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+});
+
+
+// server.start();
+
+// console.log("packageDef : " , packageDef ) ;
+// console.log("grcObjct" , grpcObject) ;
+// console.log("todoPackage" , todoPackage) ;
+
+
+const toodos = [] ;
+function createTodo(call, callback) {
+    
+    const todoItem = {
+        "id" : toodos.length + 1 ,
+        "text" : call.request.text 
+    }
+
+    toodos.push(todoItem) ;
+
+    console.log(call);
 }
 
 
-function readTodos () {
+function readTodos(call, callback) {
 
 }
 
